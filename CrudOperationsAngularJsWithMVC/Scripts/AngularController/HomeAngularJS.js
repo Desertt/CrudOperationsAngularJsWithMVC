055744bf3d957eb451e3a40b25dd551039aab46b
mustafa.dagtekin <mustafa.dagtekin@outlook.com>
2019-09-12 17:54:27 +0300

    Index

--- CrudOperationsAngularJsWithMVC/Scripts/AngularController/HomeAngularJS.js
+++ CrudOperationsAngularJsWithMVC/Scripts/AngularController/HomeAngularJS.js
@@ -2,9 +2,17 @@
 app.controller("HomeController", function myfunction($scope, $http) {
     $scope.btntext = "Save";
     $scope.savedata = function () {
-
+        #scope.btntext = "Please Wait..";
         $http({
-
-        })
+            method: "POST",
+            url: '/Home/Add_record',
+            data:$scope.register
+        }).success(function (d) {
+            $scope.btntext = "Save";
+            $scope.register = null;
+            alert(d);
+            }).error(function myfunction() {
+                alert('Failed');
+            })
     }
 });
